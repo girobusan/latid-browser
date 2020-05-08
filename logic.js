3214e48e76d743b2fb8580755b98ec91f40bdce9
Gleb@home <girobusan@gmail.com>
2020-05-08 20:16:59 +0300

    initial

---
+++ logic.js
@@ -0,0 +1,242 @@
+const fs = require('fs');
+const path = require("path");
+const { dialog } = require('electron').remote;
+const electron = require("electron")
+const ipc = electron.ipcRenderer;
+const recursive = require("recursive-readdir");
+
+
+window.localFS = {
+  "type": "latid-browser"
+};
+
+//first steps, hiding excess symbols
+(function () {
+
+  //sevice functions
+  var addScript = function (p) {
+    let s = document.createElement("script");
+    s.setAttribute("src", p);
+    document.body.appendChild(s);
+  }
+
+
+
+  var readFileTextPromise = function (fp) {
+    //console.log("READ TEXT FILE" , fp);
+    return new Promise(function (ok, notOk) {
+      fs.readFile(fp, function (err, data) {
+        if (err) {
+          notOk(err)
+        } else {
+          try {
+            let r = data.toString();
+            ok(r);
+          } catch{
+            ok(data)
+          }
+          //ok(JSON.parse(data))
+        }
+      })
+    })
+  }
+  //storage
+  /*
+ //storage
+    let store = window.localStorage;
+    //chek
+    window.storeCheck = function (v) {
+      if (!store.getItem(v)) {
+        store.setItem(v, v);
+      }
+    }
+
+
+  */
+  //sites storage
+  var store = new function () {
+    var s = window.localStorage;
+    this.check = function(title, pth){
+      console.log("Checking sites history")
+      let k = title + "@" + pth
+      if (!s.getItem(k)) {
+        s.setItem(k, JSON.stringify({ "title": title, "path": pth, "key": k }));
+      }
+    }
+    this.remove = function (k) {
+      s.removeItem(k)
+    }
+
+    this.items = function () {
+      let r = [];
+      for (let i = 0; i < s.length; i++) {
+        //console.log("key" , s)
+        let k = s.key(i);
+        r.push(JSON.parse(s.getItem(k)));
+      }
+      return r;
+    }
+
+  }
+  //switch to site
+
+
+
+  //my fav function
+  var createServer = function (root) {
+    console.log("Setting up local FS");
+    console.log("Local FS server at", root);
+    
+    //process.chdir(root);
+    window.localFS.root = root; //DEPRECATED --> window.localFS.server.root
+    var makepath = function (p) {
+      return path.join(root, p);
+    }
+
+    return new function () {
+      this.root = root;
+      var my = this;
+      //refreshSrcList 
+      this.refreshSrcList = function () {
+        console.log("Refresh Src List is not implemented");
+      }
+
+      //copy
+      this.copy = function (f, t) {
+        return new Promise(function (res, rej) {
+          res(ipc.send("copy", { from: makepath(f), to: makepath(t) }));
+        })
+      }
+      //get
+      this.get = function (p) {
+        return new Promise(function (res, rej) {
+          let result = ipc.sendSync("get", { path: makepath(p) });
+          console.log("Event returned", p, result);
+          res(result);
+        })
+      }
+      //getAsText
+      this.getAsText = function (p) {
+        return readFileTextPromise(makepath(p));
+      }
+
+      this.getAsTextSync = function (p) {
+        return fs.readFileSync(makepath(p)).toString();
+      }
+
+      //list
+      this.list = function (p) {
+        //return Promise =>[{details:{path=.....}, ... }]
+        let sp = makepath(p);
+        let spl = sp.length;
+        return new Promise(
+          function (res, rej) {
+            recursive(sp, function (err, files) {
+              if (err) {
+                rej(err);
+              }
+              let rf = files.map(e => e.substring(spl)).filter(a => !a.startsWith("_")).map(w => ({ "path": w }));
+              res({ details: rf });
+
+            })
+          }
+        )
+      }
+      //init
+      this.init = function () {
+        return new Promise(
+          function (res, rej) {
+            res(my)
+          }
+        )
+      }
+      //write
+      this.write = function (c, p) {
+        return new Promise(function (res, rej) {
+          res(ipc.sendSync("write", { path: makepath(p), content: c }));
+        })
+      }
+      //writeOutput
+      //-->return this.write(cnt , Util.gluePath(l4.settings.output.dir , pth));
+    }//server fn ends
+    //h
+  }//it was make server
+
+  var loadSite = function (locp) {
+    console.log("Loading site" , locp);
+    //EXPERIMENTUM
+    //history.pushState(locp , null , locp)
+    //check dir
+    if (fs.existsSync(locp) && fs.existsSync(path.join(locp, "_config/settings.json"))) {
+      //get site info
+      try {
+        var settings = JSON.parse(fs.readFileSync(path.join(locp, "_config/settings.json")));
+        console.log(settings);
+        var title = settings.site.title;
+        console.log("Site is" , title , "at" , locp)
+      } catch(err){
+        console.error("can not load settings from" , path.join(locp, "_config/settings.json"));
+        console.error(err);
+        return false
+      }
+      //check storage
+      store.check(title, locp);
+      //create server
+      window.localFS.server = createServer(locp);
+      let preload = document.createElement("div");
+      preload.id="preload";
+      document.body.appendChild(preload);
+      addScript(path.join(window.localFS.root, "_system/scripts/l4.js"));
+      return true;
+    } else {
+      console.error("Required files do not exist")
+      return false
+    }
+  }
+
+  window.addEventListener("DOMContentLoaded", function () {
+    let saved_items_table = document.getElementById("saved_sites");
+    //show already saved sites
+    store.items().forEach(function (e) {
+      //console.log(e);
+      //row
+      let row = document.createElement("div");
+      row.classList.add("site");
+      //run button
+      let rb = document.createElement("div");
+      rb.setAttribute("class", "run");
+      rb.innerHTML = `<div class='part label'>${e.title}</div><div class='part path'>${e.path}</div>`;
+      rb.addEventListener("click", function (evt) {
+        if (!loadSite(e.path)) {
+          console.error("Can not load site");
+        };
+      })
+      row.appendChild(rb);
+      //minus button
+      let minus = document.createElement("div");
+      minus.setAttribute("class", "store remove");
+      minus.innerHTML = "×";
+      minus.addEventListener("click", function (e) {
+        store.remove(e.key);
+        row.remove();
+      });
+      row.appendChild(minus);
+      saved_items_table.appendChild(row);
+    });
+    ///-storage
+    let b = document.getElementById("main_button");
+    b.addEventListener("click", function () {
+      let p = dialog.showOpenDialog({
+        properties: ['openDirectory']
+      }).then(function (r) {
+        //init server
+        if (r.filePaths.length == 0) {
+          return;
+        }
+        loadSite(r.filePaths[0]);
+      }
+      );
+    })
+  })
+}());
+
---
+++ main.js
@@ -0,0 +1,84 @@
+const { app, BrowserWindow } = require('electron');
+const ipc = require('electron').ipcMain;
+const fs = require('fs');
+const path = require("path");
+var win;
+
+//server funcs
+//server write
+ipc.on('write', function (event, arg) {
+  let where = path.dirname(arg.path);
+  if (!fs.existsSync(where)) {
+    fs.mkdirSync(where, { recursive: true }, (err) => { console.error(err) });
+  }
+  fs.writeFile(arg.path, arg.content, () => event.returnValue = { 'success': 'yes' });
+
+});
+//server copy
+ipc.on('copy', function (event, arg) {
+  let where = path.dirname(arg.to);
+  if (!fs.existsSync(where)) {
+    fs.mkdirSync(where, { recursive: true }, (err) => { console.error(err) });
+  }
+  fs.copyFile(arg.from, arg.to, () => event.returnValue = { 'success': 'yes' });
+
+});
+//server get
+ipc.on('get', function (event, arg) {  
+    fs.readFile(arg.path,  function(err, data) { 
+      if (err) {console.error(err); event.returnValue = {"success" : "no"}};
+      let r = data;
+      try{
+        r = JSON.parse(data);
+      }catch{
+        //do nothing
+      }
+      event.returnValue = r;     
+    });
+});
+//server get-as-text
+ipc.on('get-as-text', function (event, arg) {  
+  fs.readFile(arg.path,  function(err, data) { 
+    if (err) {console.error(err); event.returnValue = {"success" : "no"}};
+    let r = data.toString();    
+    event.returnValue = r;     
+  });
+});
+
+
+
+
+//end server logic
+
+function createWindow() {
+  // Создаем окно браузера.
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // и загрузить index.html приложения.
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  // Для приложений и строки меню в macOS является обычным делом оставаться
+  // активными до тех пор, пока пользователь не выйдет окончательно используя Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', () => {
+  // На MacOS обычно пересоздают окно в приложении,
+  // после того, как на иконку в доке нажали и других открытых окон нету.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+});
